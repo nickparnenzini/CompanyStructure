cee49233d8a5091dc17e97afca8eb13fbb2dfedd
vparnenzini <valeria-parnenzini@users.noreply.github.com>
2018-03-08 23:57:09 +0000

    Update SoftwareEngineer.java

--- SoftwareEngineer.java
+++ SoftwareEngineer.java
@@ -1,14 +1,13 @@
 public class SoftwareEngineer extends TechnicalEmployee {
 
     private boolean codeAccess;
-    private Employee manager;
     private double bonus;
 
     public SoftwareEngineer(String name) {
         super(name,75000);
         this.codeAccess = false;
-        this.manager = null;
         this.bonus = 0.0;
+        super.isManager(false);
     }
 
     public boolean getCodeAccess() {
@@ -20,11 +19,11 @@ public class SoftwareEngineer extends TechnicalEmployee {
     }
 
     public void setManager(TechnicalLead t) {
-        this.manager = t;
+        super.setManager(t);
     }
 
     public Employee getManager() {
-        return this.manager;
+        return super.getManager();
     }
 
     public double getBonus() {
@@ -39,11 +38,20 @@ public class SoftwareEngineer extends TechnicalEmployee {
         return super.getSuccessfulCheckIns();
     }
 
-    public String employeeStatus() {
-
+    public boolean checkInCode() {
+        TechnicalLead t = (TechnicalLead)(super.getManager());
+        if (t.approveCheckIn(this)) {
+            super.increaseSuccessfulCheckIns();
+            this.codeAccess = true;
+            return true;
+        }
+        else {
+            this.codeAccess = false;
+        }
+        return false;
     }
 
-    public int getSuccessfulCheckIns() {
-        
+    public String employeeStatus() {
+       return super.employeeStatus();
     }
 }
