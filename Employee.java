c348f65cd04e58e259ba2369ca648896c5e8dcf7
vparnenzini <valeria-parnenzini@users.noreply.github.com>
2018-03-08 23:58:16 +0000

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -2,18 +2,31 @@ public abstract class Employee {
 
     private String name;
     private double baseSalary;
+    private double bonus;
     private int employeeID;
     private static int employees = 0;
+    private Employee manager;
+    private boolean isManager;
 
     public Employee(String name, double baseSalary) {
         this.name = name;
         this.baseSalary = baseSalary;
-        this.employeeID = employees;
-        employees++;
+        this.bonus = 0.0;
+        this.employeeID = employees++;
+        this.manager = null;
+        this.isManager = false;
     }
 
     public double getBaseSalary() {
         return this.baseSalary;
+    }
+
+    public double getBonus() {
+        return this.bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
     }
 
     public String getName() {
@@ -24,7 +37,21 @@ public abstract class Employee {
         return this.employeeID;
     }
 
-    public abstract Employee getManager();
+    public Employee getManager() {
+        return this.manager;
+    }
+
+    public void setManager(Employee manager) {
+        this.manager = manager;
+    }
+
+    public boolean isManager() {
+        return this.isManager;
+    }
+
+    public void isManager(boolean value) {
+        this.isManager = value;
+    }
 
     public boolean equals(Employee other) {
         return this.employeeID == other.getEmployeeID();
