3c4aac036ec9b13357c32a829e1dec62e3295fdd
vparnenzini <valeria-parnenzini@users.noreply.github.com>
2018-03-08 23:54:21 +0000

    Update Accountant.java

--- Accountant.java
+++ Accountant.java
@@ -3,13 +3,11 @@ import java.util.LinkedList;
 public class Accountant extends BusinessEmployee {
 
     private TechnicalLead teamSupported;
-    private double bonusBadget;
     private Employee manager;
 
     public Accountant(String name) {
         super(name,50000);
         this.teamSupported = null;
-        this.bonusBadget = 0.0;
         this.manager = null;
     }
 
@@ -18,7 +16,7 @@ public class Accountant extends BusinessEmployee {
     }
 
     public double getBonusBadget() {
-        return this.bonusBadget;
+        return super.getBonusBadget();
     }
 
     public Employee getManager() {
@@ -32,19 +30,20 @@ public class Accountant extends BusinessEmployee {
             bonusBadget += s.getBaseSalary();
         }
         bonusBadget += (bonusBadget)*0.1;
-        this.bonusBadget = bonusBadget;
+        super.setBonusBudget(bonusBadget);
     }
 
     public boolean approveBonus(double bonus) {
-        if ( (teamSupported == null) || (bonus > this.bonusBadget)) {
+        if ( (teamSupported == null) || (bonus > super.getBonusBadget())) {
             return false;
         }
-        this.bonusBadget -= bonus;
+        double newBadget = super.getBonusBadget() - bonus;
+        super.setBonusBudget(newBadget);
         return true;
     }
 
     public String employeeStatus() {
-        String result = super.toString() + " with a budget of " + Double.toString(this.bonusBadget) + " is supporting " + teamSupported.getName();
+        String result = super.toString() + " with a budget of " + Double.toString(super.getBonusBadget()) + " is supporting " + teamSupported.getName();
         return result;
     }
 }
